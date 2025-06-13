7b778ec3fd67ed9f4f95bc57b249bbdab9d892b3
KhoiLuu143 <khoiluub143@gmail.com>
2025-06-13 10:22:37 +0700

    minor bug fix

--- src/main/java/com/gemstore/gemstone_store/repository/LoaiDichVuRepository.java
+++ src/main/java/com/gemstore/gemstone_store/repository/LoaiDichVuRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface LoaiDichVuRepository extends JpaRepository<LoaiDichVu, String> {
-    List<LoaiDichVu> findByTenDichVuContainingIgnoreCase(String name);
+    List<LoaiDichVu> findByTenLDVContainingIgnoreCase(String name);
 }
